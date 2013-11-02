f5d0c3f9075dcf57f177fd1a57ad4c58e66fa691
jaseg <jaseg@jaseg.net>
2013-11-02 23:09:18 +0100

    Extended for simultaneous read/write

--- button.c
+++ button.c
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <usb.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 #include "button.h"
 
@@ -44,10 +45,17 @@ struct usb_dev_handle *initPanel(unsigned int devnum) {
 		printf("Did you perhaps forget to sudo or add a udev rule?\n");
 		return NULL;
 	}
+	usb_set_debug(1);
+	if(usb_claim_interface(GPhandle, 0) < 0){
+		fprintf(stderr, "ERROR: Cannot claim interface!\n");
+		return NULL;
+	}
 	return GPhandle;
 }
 
 int main(int argc, char **argv) {
+	int ec = 0;
+
 	struct usb_dev_handle *dev = initPanel(0);
 	if(dev == NULL) {
 		fprintf(stderr, "ERROR: Couldn't initialize USB button!\n");
@@ -55,21 +63,24 @@ int main(int argc, char **argv) {
 	}
 
 	if(argc == 2){
-		unsigned char cmd = 0;
-		if (strcmp("open", argv[1])) {
+		unsigned char cmd = 64;
+		if (!strcmp("open", argv[1])) {
 			cmd = 96; //alternatively: 224
-		} else if(strcmp("close", argv[1])) {
+		} else if(!strcmp("close", argv[1])) {
 			cmd = 80; //alternatively: 208
 		} else {
 			printf("Please specify a command (open/close)\n");
 			return 1;
 		}
-		usb_interrupt_write(dev, 0x02, &cmd, 1, 1000);
 	}else if(argc == 1){
 		unsigned char data;
 		unsigned char last = 0;
+		unsigned int state = 0;
+
+		fcntl(0, F_SETFL, fcntl(0, F_GETFL, 0) | O_NONBLOCK);
+
 		while(1){
-			if(!usb_interrupt_read(dev, 0x01, &data, 1, 1000)){
+			if(!usb_interrupt_read(dev, 0x01, &data, 1, 1)){
 				//printf("%x\n", data);
 				unsigned char c = 64;
 				if(last != data){
@@ -87,9 +98,32 @@ int main(int argc, char **argv) {
 						printf("closed\n");
 					last = data;
 				}
-				usb_interrupt_write(dev, 0x02, &c, 1, 1000);
-				usleep(100000);
+				if((ec = usb_interrupt_write(dev, 0x02, &c, 1, 1)) < 0){
+					printf("Error writing to USB device (%d): %s\n", ec, usb_strerror());
+					return 2;
+				}
 			}
+			int len = read(0, &data, 1);
+			if(len > 0){
+				char cmd;
+				if(state == 0){
+					state = 1;
+					if(data == 'c'){
+						cmd = 96;
+					}else if(data == 'o'){
+						cmd = 80;
+					}
+					if((ec = usb_interrupt_write(dev, 0x02, &cmd, 1, 1)) < 0){
+						printf("Error writing to USB device (%d): %s\n", ec, usb_strerror());
+						return 2;
+					}
+				}else{
+					if(data == '\n'){
+						state = 0;
+					}
+				}
+			}
+			usleep(100000);
 		}
 	}else{
 		printf("Invalid argument. Specify open/close to open/close or nothing to listen.\n");
